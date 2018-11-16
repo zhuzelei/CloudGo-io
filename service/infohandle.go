cc52dd39eb304509d45e1dad8c0be466cc67a588
NameZZL <31604203+zhuzelei@users.noreply.github.com>
2018-11-16 18:27:32 +0800

    Update infohandle.go

--- service/infohandle.go
+++ service/infohandle.go
@@ -10,7 +10,6 @@ func InfoHandler(formatter *render.Render) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
 		req.ParseForm()
-		//输出匿名结构并json序列化输出
 		formatter.HTML(w, http.StatusOK, "table", struct {
 			Name  string
 			Phone string
