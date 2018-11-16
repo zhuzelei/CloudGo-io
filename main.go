7e8c8153b106b9a9cd9070cdfd9236419f520d73
NameZZL <31604203+zhuzelei@users.noreply.github.com>
2018-11-16 18:29:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,18 +11,15 @@ import (
 )
 
 const (
-	//PORT 默认的端口号
 	PORT string = "8080"
 )
 
 func main() {
-	// 从系统环境变量中获得端口号
 	port := os.Getenv("PORT")
-	if len(port) == 0 { //如果没有PORT系统变量，则为默认端口号
+	if len(port) == 0 { 
 		port = PORT
 	}
 
-	// 从命令行输入中获取端口号
 	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
 	flag.Parse()
 	if len(*pPort) != 0 {
@@ -32,29 +29,16 @@ func main() {
 	server := service.NewServer()
 	server.Run(":" + port)
 
-	//优雅的退出 否则直接ctrl+c 端口8080会持续被占用
 
-	// Go signal notification works by sending `os.Signal`
-	// values on a channel. We'll create a channel to
-	// receive these notifications (we'll also make one to
-	// notify us when the program can exit).
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
-	// `signal.Notify` registers the given channel to
-	// receive notifications of the specified signals.
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	// This goroutine executes a blocking receive for
-	// signals. When it gets one it'll print it out
-	// and then notify the program that it can finish.
 	go func() {
 		sig := <-sigs
 		fmt.Println()
 		fmt.Println(sig)
 		done <- true
 	}()
-	// The program will wait here until it gets the
-	// expected signal (as indicated by the goroutine
-	// above sending a value on `done`) and then exit.
 	fmt.Println("awaiting signal")
 	<-done
 
