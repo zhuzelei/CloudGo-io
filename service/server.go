1ceccc8fa634ac6978e81d4c29a02e70c986ea99
NameZZL <31604203+zhuzelei@users.noreply.github.com>
2018-11-16 18:28:20 +0800

    Update server.go

--- service/server.go
+++ service/server.go
@@ -6,28 +6,26 @@ import (
 	"net/http"
 	"os"
 
-	//需要引入三个packet
-	"github.com/codegangsta/negroni" //库
-	"github.com/gorilla/mux"         //路由功能
+	
+	"github.com/codegangsta/negroni" 
+	"github.com/gorilla/mux"         
 	"github.com/unrolled/render"
 )
 
 // NewServer configures and returns a Server.
 func NewServer() *negroni.Negroni {
-	//formatter构造，指定模板的目录，模板名字的后缀名
 	formatter := render.New(render.Options{
-		//设置
 		Directory:  "templates",
 		Extensions: []string{".html"},
 		IndentJSON: true,
 	})
 
-	n := negroni.Classic() //经典实例，提供通用必要的属性
+	n := negroni.Classic() 
 	mx := mux.NewRouter()
 
 	initRoutes(mx, formatter)
 
-	n.UseHandler(mx) //引入处理器
+	n.UseHandler(mx) 
 	return n
 }
 
@@ -42,7 +40,6 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 		}
 	}
 
-	//使用处理器处理不同后缀
 	mx.HandleFunc("/unknown", UnknownHandler(formatter))
 	mx.HandleFunc("/table", InfoHandler(formatter))
 	mx.HandleFunc("/js", JsHandler(formatter)).Methods("GET")
